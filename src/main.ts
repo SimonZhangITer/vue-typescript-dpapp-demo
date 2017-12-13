204f439fc315c6f351f6078c6ab83e0be77533fe
张韧 <zhangren@meituan.com>
2017-12-13 11:08:53 +0800

    add lazyload

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueLazyload from 'vue-lazyload'
+
+Vue.use(VueLazyload)
 
 Vue.config.productionTip = false
 
--- src/typings/tools.d.ts
+++ src/typings/tools.d.ts
@@ -2,3 +2,5 @@ declare module 'vue-awesome-swiper' {
   export const swiper: any
   export const swiperSlide: any
 }
+
+declare module 'vue-lazyload'
