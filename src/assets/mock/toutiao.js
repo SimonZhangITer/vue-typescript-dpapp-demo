e7da67e76f151fdf0cd092fd01d62b12d34c3a6f
张韧 <zhangren@meituan.com>
2017-12-13 10:41:53 +0800

    add toutiao json

---
+++ src/assets/mock/toutiao.js
@@ -0,0 +1,34 @@
+export default {
+  'list': [
+    {
+      'url': '//h5.dianping.com/app/h5-ranklist-static/list_nearby.html?headlineId=3700104&source=weixinM&from=headline',
+      'title': '绚景楼藏惊喜：有公主陪伴的圣诞午餐会有多神奇',
+      'imgUrl': 'https://img.meituan.net/dpgroup/a2c05c7fdc804cf414854b3a5ba2fd4e231267.jpg%40100w_100h_1e_1c_1l%7Cwatermark%3D1%26%26r%3D1%26p%3D9%26x%3D20%26y%3D20',
+      'imgNum': '1'
+    },
+    {
+      'url': '//h5.dianping.com/app/h5-ranklist-static/list_nearby.html?headlineId=3278443&source=weixinM&from=headline',
+      'title': '老娘舅17年来第一封致歉信',
+      'imgUrl': 'https://img.meituan.net/dpgroup/7b571b075b7f42e55c061615e9f65b57633847.jpg%40100w_100h_1e_1c_1l%7Cwatermark%3D1%26%26r%3D1%26p%3D9%26x%3D20%26y%3D20',
+      'imgNum': '17'
+    },
+    {
+      'url': '//h5.dianping.com/app/h5-ranklist-static/list_nearby.html?headlineId=3369501&source=weixinM&from=headline',
+      'title': '揭秘！老娘舅好吃的米饭是这样做成的 | 至纯源味',
+      'imgUrl': 'https://img.meituan.net/dpgroup/7b571b075b7f42e55c061615e9f65b57633847.jpg%40100w_100h_1e_1c_1l%7Cwatermark%3D1%26%26r%3D1%26p%3D9%26x%3D20%26y%3D20',
+      'imgNum': '35'
+    },
+    {
+      'url': '//h5.dianping.com/app/h5-ranklist-static/list_nearby.html?headlineId=3678071&source=weixinM&from=headline',
+      'title': '假如维密天使们也做美甲，画风应该是这样的…',
+      'imgUrl': 'https://p0.meituan.net/gpa/d4322e5f9e8b621dd437079cb548f1a093719.jpg%40100w_100h_1e_1c_1l%7Cwatermark%3D1%26%26r%3D1%26p%3D9%26x%3D20%26y%3D20',
+      'imgNum': '77'
+    },
+    {
+      'url': '//h5.dianping.com/app/h5-ranklist-static/list_nearby.html?headlineId=3706721&source=weixinM&from=headline',
+      'title': '临近圣诞，怎么能少了一款圣诞美甲？',
+      'imgUrl': 'https://p1.meituan.net/poicontent/1d9c7614c2d01795ea271bac1a3e78d864323.jpg%40100w_100h_1e_1c_1l%7Cwatermark%3D1%26%26r%3D1%26p%3D9%26x%3D20%26y%3D20',
+      'imgNum': '29'
+    }
+  ]
+}
