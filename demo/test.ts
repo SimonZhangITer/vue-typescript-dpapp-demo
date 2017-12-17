6ac06eac44f6aa7785d2afcd824bd954b705c11a
张韧 <zhangren@meituan.com>
2017-12-17 17:43:24 +0800

    init

--- demo/test.ts
+++
@@ -1,9 +0,0 @@
-// import * as Util from './namespace'
-
-// Util.getParam
-
-/// <reference path="namespace.ts" />
-
-namespace Util {
-  interface Test {}
-}
--- src/typings/tools.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'vue-awesome-swiper' {
-  export const swiper: any
-  export const swiperSlide: any
-}
-
-declare module 'vue-lazyload'
